dd736bad78a5bc10ea27cd50cc682e5d411e16f8
dididthat <diananikulina00@mail.ru>
2023-08-24 17:12:43 +0300

    Initial Commit

---
+++ test_avito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test_avito
+//
+//  Created by Diana Nikulina on 24.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
